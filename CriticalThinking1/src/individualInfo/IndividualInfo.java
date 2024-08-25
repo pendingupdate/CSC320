90812d1c6b5b6f6daccc6c027b2dc66284c89da6
Stephanie Peets <stephanie.peets@csuglobal.edu>
2024-08-25 01:14:08 -0400

    delete eclipse folder

---
+++ CriticalThinking1/src/individualInfo/IndividualInfo.java
@@ -0,0 +1,24 @@
+package individualInfo;
+
+public class IndividualInfo {
+
+	public static void main(String[] args) {
+		String firstName;
+		String lastName;
+		String streetAddress;
+		String city;
+		String zipCode;
+		
+		firstName = "Aliciana";
+		lastName = "Smaithe";
+		streetAddress = "7134 Springfeldston Boulevard";
+		city = "Springfeldston, MN";
+		zipCode = "98765";
+
+		System.out.println(firstName + " " + lastName);
+		System.out.println(streetAddress);
+		System.out.println(city + " " + zipCode);
+		
+	}
+
+}
--- eclipse-workspace/CriticalThinking1/src/module-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module CriticalThinking1 {
-}
